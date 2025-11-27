9a1e3a07c06b92c01f62e36c344eaec4cd9c6fc2
eggunof <eggunof@gmail.com>
2025-11-27 19:30:22 +0300

    Update regex to allow underscores in predicate names for better parsing.

--- nslps/formal_language/parsing/formula_parser.py
+++ nslps/formal_language/parsing/formula_parser.py
@@ -50,7 +50,7 @@ class FormulaParser:
             literal_str = literal_str[1:]
 
         # Find predicate name and arguments within parentheses
-        match = re.match(r"([A-Za-zА-Яа-я]+)\((.*)\)", literal_str)
+        match = re.match(r"([A-Za-zА-Яа-я_]+)\((.*)\)", literal_str)
         if not match:
             raise FormulaParserException(f"Invalid predicate format: {literal_str}")
 
